0f609b4b95e770e904e052495a9276d2f81d3c20
Palma Luna Melissa <160922506+PalmaMel@users.noreply.github.com>
2024-05-04 08:12:19 -0600

    trabajo en clase

--- dna_analysis/utils/validators.py
+++ dna_analysis/utils/validators.py
@@ -75,3 +75,8 @@ def check_sequence_length(sequence, min_length=100):
     if len(sequence) < min_length:
         raise ValueError(f"La secuencia debe tener al menos {min_length} bases.")
     return True
+
+if __name__ == "__main__":
+    seq="ATCGACGCGTGA"
+    assert (validate_dna_sequence(seq))==True
+    print ("es correcto")
